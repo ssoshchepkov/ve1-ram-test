02d1466f1afc5341ab0d0a7a49749bab7a396f9b
Semyon Oshchepkov <s.s.oshchepkov@gmail.com>
2018-04-15 22:45:02 +1000

    added reset of I register in interrupt handler, comments translated in Russian

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ve1-ram-test
-RAM test for the German controller Verarbeitungseinheit VE1. Written in [Zeus Z80 Assembler](http://www.desdes.com/products/oldfiles/zeus.htm).
+RAM test for the German controller Verarbeitungseinheit VE1. Written in [Zeus Z80 Assembler](http://www.desdes.com/products/oldfiles/zeus.htm). Comments a written in Russian because of a customer request.
--- memory_test.asm
+++ memory_test.asm
@@ -1,140 +1,182 @@
-WriteIndicatorPort      equ 8
-ReadIndicatorPort       equ 12
-RamStartAddress         equ 3072
-RamEndAddress           equ 4096
-RamSize                 equ RamEndAddress - RamStartAddress
-RomStartAddress         equ 0
-RomModuleSize           equ 1024
-BinDumpFileName         equ "memory_test.bin"
+WriteIndicatorPort      equ 8                               ; порт индикаторов, отображающих записанный байт
+ReadIndicatorPort       equ 12                              ; порт индикаторов, отображающих считанный байт
+RamStartAddress         equ 3072                            ; адрес начала RAM
+RamEndAddress           equ 4096                            ; адрес конца RAM
+RamSize                 equ RamEndAddress - RamStartAddress ; размер RAM
+RomStartAddress         equ 0                               ; адрес начала ROM
+RomModuleSize           equ 1024                            ; размер микросхемы ROM
+BinDumpFileName         equ "memory_test.bin"               ; имя файла в который записывается бинарный дапм
 
+; 4 разных значения, которые записываются/считываются при выполнении тестов
 TestByteValue1          equ 0
 TestByteValue2          equ 255
 TestByteValue3          equ 10101010b
 TestByteValue4          equ 01010101b
 
+; 4 порта таймера Z80 CTC
 CtcChannel0             equ 0
 CtcChannel1             equ 1
 CtcChannel2             equ 2
 CtcChannel3             equ 3
 
+InterruptVectorTable    equ 10000b                          ; Начало таблицы векторов прерываний Z80 CTC
+Channel3VectorOffset    equ 110b                            ; Смещение вектора для канала 3, определяемое 1м и 2м
+                                                            ; битами
+
+; макрос вызова процедуры теста
+; параметры:
+; testByte - значение (байт), используемое для записи и проверки в данном тесте
+; testProc - адрес процедуры проверки памяти
 callTest                macro(testByte, testProc)
                         ld a, testByte
                         call testProc
                         jr nz, Exit
                         mend
 
+; макрос, выставляющий одинаковое значение на обоих светодиодных индикаторах
+; параметры:
+; value - значение (байт), которое выставляется на обоих индикаторах
 setIndicators           macro(value)
                         ld a, value
                         out (WriteIndicatorPort), a
                         out (ReadIndicatorPort), a
                         mend
 
-                        org RomStartAddress
-                        jr InitCtc
+                        org RomStartAddress                 ; начало ROM
+                        jr InitCtc                          ; пропускаем таблицу прерываний и переходим к 
+                                                            ; инициализации таймера
 
-                        org 16h
-                        DEFW CT3_ZERO                   ; interrupt vector
+                        org InterruptVectorTable + Channel3VectorOffset ; адрес вектора для канала 3
+                        DEFW Channel3Vector                 ; вектор прерывания канала 3
 
 InitCtc:
-;init CH 0 and 1
-                        ld a, 00000011b                 ; int off, timer on, prescaler=16, don't care ext. TRG edge,
-                                                        ; start timer on loading constant, no time constant follows
-                                                        ; sw­rst active, this is a ctrl cmd
-                        out (CtcChannel0),A             ; CH0 is on hold now
-                        out (CtcChannel1),A             ; CH1 is on hold now
+; Инициализация каналов 0 and 1
+                        ld a, 00000011b                 ; параметры каналов (7-0 биты): прерывания выкл., режим
+                                                        ; таймера, делитель = 16, CLK/TRG Edge = Falling Edge, 
+                                                        ; таймер запускается после загрузки константы, константа
+                                                        ; не передается после выполнения этой команды, программный
+                                                        ; reset включен, это команда управления
+                        out (CtcChannel0),A             ; Выполняем команду для канала 0. Канал не используется.
+                        out (CtcChannel1),A             ; Выполняем команду для канала 1. Канал не используется.
 
-;init CH2
-;CH2 divides CPU CLK by (256*256) providing a clock signal at TO2. TO2 is connected to TRG3.
-                        ld A,00100111b                  ; int off, timer on, prescaler=256, no ext. start,
-                                                        ; start upon loading time constant, time constant follows
-                                                        ; sw reset, this is a ctrl cmd
+; Инициализация канала 2
+; Канал  делит CPU CLK на (256*256) передавая сигнал на TO2. TO2 подсоеденино TRG3.
+                        ld A,00100111b                  ; параметры каналов (7-0 биты): прерывания выкл., режим
+                                                        ; таймера, делитель = 256, CLK/TRG Edge = Falling Edge, 
+                                                        ; таймер запускается после загрузки константы, константа
+                                                        ; передается после выполнения этой команды, программный
+                                                        ; reset включен, это команда управления
                         out (CtcChannel2), A
-                        ld A, 0FFh                      ; time constant 255d defined
-                        out (CtcChannel2), A            ; and loaded into channel 2
-                                                        ; T02 outputs f= CPU_CLK/(256*256)
-;init CH3
-;input TRG of CH3 is supplied by clock signal from TO2
-;CH3 divides TO2 clock by AFh
-;CH3 interupts CPU appr. every 2 (5?) sec to service int routine CT3_ZERO
-                        ld A, 11000111b                 ; int on, counter on, prescaler don't care, edge don't care,
-                                                        ; time trigger don't care, time constant follows
-                                                        ; sw reset, this is a ctrl cmd
+                        ld A, 0FFh                      ; задаем временную констанну равную 255
+                        out (CtcChannel2), A            ; для канала 2
+                                                        ; T02 выход = CPU_CLK/(256*256)
+; Инициализация канала 3
+; на вход TRG канала 3 поступает сигнал из TO2
+; Канал 3 делит TO2 на AFh
+; Канал 3 вызывает прерывание CPU. В примере, написанном для часов CPU 5 MHz указывалось, что при заданных
+; константах прерывание будет вызываться приблизительно раз в 2 секунды.
+; Обработчик прерывания находится по адресу Channel3Vector
+                        ld A, 11000111b                 ; параметры каналов (7-0 биты): прерывания вкл., режим
+                                                        ; счетчика, делитель не важен, CLK/TRG Edge = Falling Edge, 
+                                                        ; таймер не важен, константа передается после выполнения этой 
+                                                        ; команды, программный reset включен, это команда управления
                         out (CtcChannel3), A
-                        ld A, 0AFh                      ; time constant AFh defined
-                        out (CtcChannel3), A            ; and loaded into channel 3
-                        ld A, 10h                       ; it vector defined in bit 7­3,bit 2­1 don't care, bit 0 = 0
-                        out (CtcChannel0), A            ; and loaded into channel 0
+                        ld A, 0AFh                      ; задаем временную константу, равную AFh
+                        out (CtcChannel3), A            ; для канала 3
+                        ld A, InterruptVectorTable      ; вектор прерывания задается битами 7­-3
+                                                        ; биты 2 и 1 не важны, т.к. будут выставляться автоматически 
+                                                        ; в зависимости от канала, бит 0 указывает, что эта команда
+                                                        ; задает уектор прерывания
+                        out (CtcChannel0), A            ; загружаем вектор для канала 0
 
-INT_INI:
                         ld A, 0
-                        ld I, A                         ;load I reg with zero
-                        im 2                            ;set int mode 2
-                        ei                              ;enable interupt
+                        ld I, A                         ; обнулим регистр I
+                        im 2                            ; режим прерываний 2
+                        ei                              ; включаем прерывания
 
-                        setIndicators(255)
+                        setIndicators(255)              ; включаем все светодиоды на обоих индикаторах
+                                                        ; перед выполнением тестов
 
-                        halt                            ; waiting for timer interrupt
+                        halt                            ; ждем прерывание от таймера
 
+; выполняем тесты, в которых происходит побайтная запись и чтение:
                         callTest(TestByteValue1, ByteCheck)
                         callTest(TestByteValue2, ByteCheck)
                         callTest(TestByteValue3, ByteCheck)
                         callTest(TestByteValue4, ByteCheck)
 
+; выполняем тесты, в которых память сначала заполняется, а затем побайтно проверяется
                         callTest(TestByteValue1, BlockCheck)
                         callTest(TestByteValue2, BlockCheck)
                         callTest(TestByteValue3, BlockCheck)
                         callTest(TestByteValue4, BlockCheck)
 
+; включаем все светодиоды на обоих индикаторах в знак успешного завершения тестов
                         setIndicators(255)
 
 Exit:
-                        halt                            ; end of program
+                        halt                            ; конец программы
 
-
+; подпрограмма побайтной проверки памяти
+; параметры:
+; регистр A должен содержать значение, используемое для записи и проверки
 ByteCheck:
-                        ld hl, RamStartAddress
-                        ld bc, RamSize
+                        ld hl, RamStartAddress          ; начало RAM в HL
+                        ld bc, RamSize                  ; размер RAM в BC
 ByteCompare:
-                        ld (hl), a
-                        cpi                             ; hl+=1, bc-=1, compare (hl) to a
-                                                        ; set z flag to (hl) == a; set c/v flag to bc != 0
-                        jr nz, Error                    ; if z flag is 0, then we got an error
-                        jp pe, ByteCompare              ; if c/v flag is not 0 then go check next memory cell
+                        ld (hl), a                      ; записываем содержимое A в ячейку
+                        cpi                             ; сравниваем содержимое ячейки, на которую укадывает hl
+                                                        ; hl с регистром A; затем hl+=1, bc-=1
+                                                        ; флаг z содержит резултьтат сравнения 1, если равно
+                                                        ; флаг c/v сбрасывается в 0, если bc = 0
+                        jr nz, Error                    ; если флаг z равен 0, то у нас ошибка
+                        jp pe, ByteCompare              ; если флаг c/v не 0, то переходим к следующей ячейке
 
                         ret
 
 BlockCheck:
-                        ld hl, RamStartAddress
-                        ld bc, RamSize
-                        ld (hl), a
-                        dec bc
-                        ld d, h
+; сначала заполним всю область памяти
+                        ld hl, RamStartAddress          ; начало RAM в HL
+                        ld bc, RamSize                  ; размер RAM в BC
+                        ld (hl), a                      ; запишем в начальную ячейку значение, которым надо заполнить
+                                                        ; панять 
+                        dec bc                          ; bc -= 1, т.к. 1 байт мы уже записали
+                        ld d, h                         ; DE = HL
                         ld e, l
-                        inc de
-                        ldir
+                        inc de                          ; DE += 1
+                        ldir                            ; последовательно копируем BC байт из области, начинающейся
+                                                        ; с HL в область, начинающаяся с DE, заполняя её нужным 
+                                                        ; значением
 
-                        ld hl, RamStartAddress
-                        ld bc, RamSize
+; теперь последовтельно сравниваем
+                        ld hl, RamStartAddress          ; начало RAM в HL
+                        ld bc, RamSize                  ; размер RAM в BC
 BlockCompare:
-                        cpi
-                        jr nz, Error                ; if z flag is 0, then we got an error
-                        jp pe, BlockCompare         ; if c/v flag is not 0 then go check next memory cell
+                        cpi                             ; сравниваем содержимое ячейки, на которую укадывает hl
+                                                        ; hl с регистром A; затем hl+=1, bc-=1
+                                                        ; флаг z содержит резултьтат сравнения 1, если равно
+                                                        ; флаг c/v сбрасывается в 0, если bc = 0
+                        jr nz, Error                    ; если флаг z равен 0, то у нас ошибка
+                        jp pe, BlockCompare             ; если флаг c/v не 0, то переходим к следующей ячейке
 
                         ret
 
-; error handler
+; Обработчик ошибок
 Error:
-                        out (WriteIndicatorPort), a
-                        dec hl                      ; hl was incremented after comparsion by cpi command
-                        ld a, (hl)
-                        out (ReadIndicatorPort), a
+                        out (WriteIndicatorPort), a ; выводим значение, которое мы записали
+                        dec hl                      ; hl было увеличено на 1 командой cpi, уменьшаем
+                        ld a, (hl)                  ; считываем ошибочное значение
+                        out (ReadIndicatorPort), a  ; выводим значение, которое мы считали
 
                         ret
 
-; timer interrupt handler
-CT3_ZERO:
-                        setIndicators(0)        ; turn indicators off
-                        reti                    ; return without re-enabling interrupts
+; Обработчик прерывания от таймера
+Channel3Vector:
+                        ld A, 0
+                        ld I, A                     ; обнулим регистр I
+                        setIndicators(0)            ; Выключаем индикаторы
+                        reti                        ; Возвращаемся из прерывания не включая разрешения на дальнейшие
+                                                    ; прерывания
 
-
+; при трансляции сохранить бинарный дамп с нулевого адреса размером 1К в указанный файл
 output_bin      BinDumpFileName, RomStartAddress, RomModuleSize
